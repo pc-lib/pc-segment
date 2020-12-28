313489873bacad823ff3156d70b9f7546f56bf47
jimmy <web3d@live.com>
2020-12-28 11:46:35 +0800

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'da65b4b454664c4f37982aa323e67a04c71ea80d',
+    'name' => 'pc-lib/segment',
+  ),
+  'versions' => 
+  array (
+    'pc-lib/segment' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'da65b4b454664c4f37982aa323e67a04c71ea80d',
+    ),
+  ),
+);
